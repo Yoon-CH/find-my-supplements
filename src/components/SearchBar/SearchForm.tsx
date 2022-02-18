e79d8ee8dccbcfbbed43399453bc3466b74318c8
HyunHo Lee <dlgusgh2001@naver.com>
2022-02-18 20:23:35 +0900

    ♻️ 프로젝트 리팩토링

--- src/components/SearchBar/SearchForm.tsx
+++ src/components/SearchBar/SearchForm.tsx
@@ -32,10 +32,10 @@ export const SearchForm = ({ inputValue, handleChange }: SearchFormProps) => {
 };
 
 const SearchFormContainer = styled.div`
-  width: 100%;
+  width: 50vw;
   margin-bottom: 30px;
   h3 {
-    font-size: 50px;
+    font-size: 35px;
     margin-bottom: 20px;
     @media ${DEVICE.SMALL} {
       font-size: 30px;
@@ -52,30 +52,20 @@ const Form = styled.form`
   width: 100%;
   height: 5rem;
   background-color: ${COLOR.WHITE};
+  border-radius: 5px;
 `;
 
 const TextInput = styled.input`
   flex: 1;
   outline: none;
-  font-size: 40px;
+  font-size: 30px;
+  padding-left: 20px;
   font-weight: bold;
+  border-radius: 5px;
   &::placeholder {
     color: ${COLOR.BLACK_LIGHT};
   }
   @media ${DEVICE.SMALL} {
     font-size: 15px;
   }
-`;
-
-const SearchButton = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 5rem;
-  height: 5rem;
-  font-size: 1.5rem;
-  background-color: #43ddb2;
-  border-radius: 0 5px 5px 0;
-  color: ${COLOR.WHITE};
-  cursor: pointer;
 `;
