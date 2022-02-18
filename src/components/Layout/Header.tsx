15845a5a8a4a9dcf049b576ef2e22ca18fe0fdd0
Joy Lee <joylee.dev@gmail.com>
2022-02-18 14:05:01 +0900

    ♻ 검색창 브랜드명 필터링 추가
    💄 페이지 레이아웃 Header UI 제작
    
    💄 Search 검색창 UI 수정
    
    ✨ 페이지 로드 시 검색창 autoFocus

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { STYLE } from '@constants';
+
+export const Header = () => {
+  return (
+    <HeaderBlock>
+      <HeaderContainer>
+        <Logo>
+          <span>Find My Supplements</span>
+          <h2>나의 영양제 찾기</h2>
+        </Logo>
+        <MenuList>
+          <li>홈</li>
+          <li className="active">분석/추천</li>
+          <li>비교</li>
+        </MenuList>
+      </HeaderContainer>
+    </HeaderBlock>
+  );
+};
+
+const HeaderBlock = styled.header`
+  height: ${STYLE.HEADER_H};
+  background: #fff;
+  box-shadow: 0 4px 6px -6px #ccc;
+`;
+
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  max-width: ${STYLE.MAX_WIDTH};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 15px;
+  height: 100%;
+`;
+
+const Logo = styled.div`
+  line-height: 1;
+  span {
+    color: #555;
+    text-transform: uppercase;
+  }
+  h2 {
+    margin-top: 2px;
+    font-size: 26px;
+  }
+`;
+
+const MenuList = styled.ul`
+  display: flex;
+  li {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: 85px;
+    height: ${STYLE.HEADER_H};
+    font-size: 20px;
+    position: relative;
+    cursor: pointer;
+    & + li {
+      margin-left: 40px;
+    }
+
+    &:hover,
+    &.active {
+      font-weight: bold;
+    }
+
+    &.active::after {
+      content: '';
+      position: absolute;
+      width: 100%;
+      height: 4px;
+      background: #333;
+      bottom: 0;
+    }
+  }
+`;
